91ca76d2c1eb95ffe4b839eaca67e40d1bc1aa0c
Maik Derstappen <maik@tangopalace.derico.de>
2020-07-07 12:32:44 +0300

    add image_scale_down handler

--- src/collective/image_max_fullsize/subscribers/image_scale_down.py
+++ src/collective/image_max_fullsize/subscribers/image_scale_down.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from plone.app.contenttypes.interfaces import IImage
+from plone.scale.scale import scaleImage
 
-def handler(obj, event):
-    """ Event handler
+import transaction as zt
+
+
+def handler(obj, event=None):
     """
-    print(u"{0} on object {1}".format(event.__class__, obj.absolute_url()))
+    """
+    max_width = 1900
+    max_height = 1900
+    max_size = 1048576  # 1048576 == 1MB
+    if not IImage.providedBy(obj):
+        return
+
+    if obj.image.size <= max_size:
+        print("image size lower than max_size: {}".format(max_size))
+        return
+
+    image_dimensions = obj.image.getImageSize()
+    if image_dimensions[0] < max_width and image_dimensions[1] < max_height:
+        print("image dimensions smaler than: {}x{}".format(max_width, max_height))
+        return
+
+    data = obj.image.data
+    result = scaleImage(
+        data, width=max_width, height=max_height, direction="down", quality=90
+    )
+    obj.image._setData(result[0])
+    print("Image scaled down to {0}KB / {1}".format(obj.image.size / 1024, result[2]))
+    zt.commit()
