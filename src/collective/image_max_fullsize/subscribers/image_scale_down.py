a457a9a665d87c3802c926495d0bca813a21c07b
MrTango <md@derico.de>
2022-08-31 15:53:10 +0300

    prevent subscriber from breaking when no image is given for a leadimage

--- src/collective/image_max_fullsize/subscribers/image_scale_down.py
+++ src/collective/image_max_fullsize/subscribers/image_scale_down.py
@@ -15,19 +15,23 @@ def handler(obj, event=None):
     if not IImage.providedBy(obj):
         return
 
-    if obj.image.size <= max_size:
+    image = getattr(obj, "image", None)
+    if not image:
+        return
+
+    if image.size <= max_size:
         print("image size lower than max_size: {}".format(max_size))
         return
 
-    image_dimensions = obj.image.getImageSize()
+    image_dimensions = image.getImageSize()
     if image_dimensions[0] < max_width and image_dimensions[1] < max_height:
         print("image dimensions smaler than: {}x{}".format(max_width, max_height))
         return
 
-    data = obj.image.data
+    data = image.data
     result = scaleImage(
         data, width=max_width, height=max_height, direction="down", quality=90
     )
-    obj.image._setData(result[0])
-    print("Image scaled down to {0}KB / {1}".format(obj.image.size / 1024, result[2]))
+    image._setData(result[0])
+    print("Image scaled down to {0}KB / {1}".format(image.size / 1024, result[2]))
     zt.commit()
